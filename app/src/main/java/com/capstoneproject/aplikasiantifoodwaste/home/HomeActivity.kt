c8a3f8cbe598e746ccccf82fc02e17f0ef16178a
RadenFaris <farisramadhan345@gmail.com>
2022-05-23 04:56:37 +0700

    ngilangin bar ama tes logout

--- app/src/main/java/com/capstoneproject/aplikasiantifoodwaste/home/HomeActivity.kt
+++ app/src/main/java/com/capstoneproject/aplikasiantifoodwaste/home/HomeActivity.kt
@@ -16,6 +16,7 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        supportActionBar?.hide()
 
         auth = FirebaseAuth.getInstance()
         binding.buttonLogout.setOnClickListener {
