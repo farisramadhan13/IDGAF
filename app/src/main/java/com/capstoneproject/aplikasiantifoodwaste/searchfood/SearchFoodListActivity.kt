3277489ef88800a26912857fee6a2c3e3cb1f78c
ilham <ilhamims10@gmail.com>
2022-05-23 16:18:30 +0700

    update layout

---
+++ app/src/main/java/com/capstoneproject/aplikasiantifoodwaste/searchfood/SearchFoodListActivity.kt
@@ -0,0 +1,11 @@
+package com.capstoneproject.aplikasiantifoodwaste.searchfood
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SearchFoodListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search_food_list)
+    }
+}
