320493b8a38072730e7963949ed1e4edb9194cbf
ilham <ilhamims10@gmail.com>
2022-05-24 15:19:46 +0700

    update layout

---
+++ app/src/main/java/com/capstoneproject/aplikasiantifoodwaste/scan/ConfirmationScanFoodActivity.kt
@@ -0,0 +1,12 @@
+package com.capstoneproject.aplikasiantifoodwaste.scan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.capstoneproject.aplikasiantifoodwaste.R
+
+class ConfirmationScanFoodActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_confirmation_scan_food)
+    }
+}
