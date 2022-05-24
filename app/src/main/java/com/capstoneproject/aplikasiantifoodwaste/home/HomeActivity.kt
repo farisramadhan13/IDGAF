6be31f92de4c9dc872d87ee8e990433176f8b041
RadenFaris <farisramadhan345@gmail.com>
2022-05-25 03:50:18 +0700

    ganti ke fragment

--- app/src/main/java/com/capstoneproject/aplikasiantifoodwaste/home/HomeActivity.kt
+++ app/src/main/java/com/capstoneproject/aplikasiantifoodwaste/home/HomeActivity.kt
@@ -3,8 +3,15 @@ package com.capstoneproject.aplikasiantifoodwaste.home
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment.Companion.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.capstoneproject.aplikasiantifoodwaste.R
 import com.capstoneproject.aplikasiantifoodwaste.databinding.ActivityHomeBinding
 import com.capstoneproject.aplikasiantifoodwaste.welcome.WelcomeActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
 
 class HomeActivity : AppCompatActivity() {
@@ -17,6 +24,17 @@ class HomeActivity : AppCompatActivity() {
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
         supportActionBar?.hide()
+
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+
+        val appBarConfiguration = AppBarConfiguration.Builder(
+            R.id.navigation_home, R.id.navigation_storage, R.id.navigation_order, R.id.navigation_profile
+        ).build()
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
 
         auth = FirebaseAuth.getInstance()
 //        binding.buttonLogout.setOnClickListener {
