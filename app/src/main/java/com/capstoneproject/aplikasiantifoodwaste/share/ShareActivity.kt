ca375b077886ef957a00fe70cd5d41ea614dd01b
RadenFaris <farisramadhan345@gmail.com>
2022-06-02 14:34:16 +0700

    activity tips and share

--- app/src/main/java/com/capstoneproject/aplikasiantifoodwaste/share/ShareActivity.kt
+++ app/src/main/java/com/capstoneproject/aplikasiantifoodwaste/share/ShareActivity.kt
@@ -1,18 +1,19 @@
-package com.capstoneproject.aplikasiantifoodwaste.tips
+package com.capstoneproject.aplikasiantifoodwaste.share
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.capstoneproject.aplikasiantifoodwaste.R
-import com.capstoneproject.aplikasiantifoodwaste.databinding.ActivityTipsBinding
+import com.capstoneproject.aplikasiantifoodwaste.databinding.ActivityShareBinding
 
-class TipsActivity : AppCompatActivity() {
+class ShareActivity : AppCompatActivity() {
 
-    private lateinit var binding : ActivityTipsBinding
+    private lateinit var binding : ActivityShareBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityTipsBinding.inflate(layoutInflater)
+        binding = ActivityShareBinding.inflate(layoutInflater)
         setContentView(binding.root)
         supportActionBar?.hide()
+
     }
 }
