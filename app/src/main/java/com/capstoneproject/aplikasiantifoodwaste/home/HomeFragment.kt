59a6b913e22bee408c1da81c21f542ba234ffb64
RadenFaris <farisramadhan345@gmail.com>
2022-06-08 23:44:35 +0700

    api api membakar

--- app/src/main/java/com/capstoneproject/aplikasiantifoodwaste/home/HomeFragment.kt
+++ app/src/main/java/com/capstoneproject/aplikasiantifoodwaste/home/HomeFragment.kt
@@ -6,14 +6,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.capstoneproject.aplikasiantifoodwaste.R
 import com.capstoneproject.aplikasiantifoodwaste.databinding.FragmentHomeBinding
 import com.capstoneproject.aplikasiantifoodwaste.scan.FoodScanActivity
+import com.capstoneproject.aplikasiantifoodwaste.scan.Storage
 import com.capstoneproject.aplikasiantifoodwaste.share.ShareActivity
+import com.capstoneproject.aplikasiantifoodwaste.storage.StorageAdapter
 import com.capstoneproject.aplikasiantifoodwaste.tips.TipsActivity
+import com.google.firebase.database.*
 
 class HomeFragment : Fragment() {
 
     private lateinit var binding : FragmentHomeBinding
+    private lateinit var database : DatabaseReference
+    private lateinit var storageRecyclerView: RecyclerView
+    private lateinit var storageArrayList: ArrayList<Storage>
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -27,6 +36,13 @@ class HomeFragment : Fragment() {
             startActivity(Intent(activity, TipsActivity::class.java))
         }
 
+        storageRecyclerView = view.findViewById(R.id.horizontal_rv)
+        storageRecyclerView.layoutManager = LinearLayoutManager(activity, LinearLayoutManager.HORIZONTAL, false)
+        storageRecyclerView.setHasFixedSize(true)
+
+        storageArrayList = arrayListOf<Storage>()
+        getStorageData()
+
         //binding.etSearchHome.searchButtonImage.
     }
 
@@ -36,5 +52,22 @@ class HomeFragment : Fragment() {
     ): View{
         binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
+    }
+    private fun getStorageData(){
+        database = FirebaseDatabase.getInstance().getReference("Storage")
+        database.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if(snapshot.exists()){
+                    for(idSnapshot in snapshot.children){
+                        val storage = idSnapshot.getValue(Storage::class.java)
+                        storageArrayList.add(storage!!)
+                    }
+                    storageRecyclerView.adapter = ListPenyimpananHomeAdapter(storageArrayList)
+                }
+            }
+            override fun onCancelled(error: DatabaseError) {
+//
+            }
+        })
     }
 }
