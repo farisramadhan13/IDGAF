e272e9be831c5f6e37439dd30aef0afdf1cd3ee5
ilham <ilhamims10@gmail.com>
2022-06-11 13:41:47 +0700

    splash biar masuk ke home kalau udah login

--- app/src/main/java/com/capstoneproject/aplikasiantifoodwaste/splash/SplashActivity.kt
+++ app/src/main/java/com/capstoneproject/aplikasiantifoodwaste/splash/SplashActivity.kt
@@ -6,11 +6,16 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import com.capstoneproject.aplikasiantifoodwaste.databinding.ActivitySplashBinding
+import com.capstoneproject.aplikasiantifoodwaste.home.HomeActivity
 import com.capstoneproject.aplikasiantifoodwaste.welcome.WelcomeActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class SplashActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivitySplashBinding
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,8 +24,16 @@ class SplashActivity : AppCompatActivity() {
         supportActionBar?.hide()
 
         Handler(Looper.getMainLooper()).postDelayed({
-            startActivity(Intent(this, WelcomeActivity::class.java))
-            finish()
+            auth = FirebaseAuth.getInstance()
+            if(auth.currentUser != null){
+                startActivity(Intent(this@SplashActivity, HomeActivity::class.java))
+                finish()
+            }
+            else{
+                startActivity(Intent(this, WelcomeActivity::class.java))
+                finish()
+            }
         }, 3000)
+
     }
 }
