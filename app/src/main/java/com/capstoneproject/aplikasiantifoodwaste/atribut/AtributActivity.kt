32fafdbf38dd9a6df49fc8dce1b9895d5f41dd6a
RadenFaris <farisramadhan345@gmail.com>
2022-06-13 00:59:00 +0700

    atribusi

---
+++ app/src/main/java/com/capstoneproject/aplikasiantifoodwaste/atribut/AtributActivity.kt
@@ -0,0 +1,12 @@
+package com.capstoneproject.aplikasiantifoodwaste.atribut
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.capstoneproject.aplikasiantifoodwaste.R
+
+class AtributActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_atribut)
+    }
+}
